d01686e525af4153225660722d4785e05f9accfe
dwkim <dwkim@uclick.co.kr>
2021-07-04 16:47:57 +0900

    2742 기찍 N

---
+++ example/210704_for_06.cs.ex
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            StringBuilder test = new StringBuilder();
+
+            for (int i=T; i>0; i--) {
+                test.Append(i);
+                test.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(test.ToString());
+        }
+    }
+}
