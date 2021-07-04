566a0432f8ef9ec25a66488d85d6e78e03ec531d
dwkim <dwkim@uclick.co.kr>
2021-07-04 17:10:11 +0900

    2438 별 찍기 - 1

---
+++ example/210704_for_09.cs.ex
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i=1; i<=T; i++) {
+                for (int j=0; j<i; j++) {
+                    result.Append("*");
+                }
+                result.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
