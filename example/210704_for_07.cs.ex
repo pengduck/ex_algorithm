9fde8313db4c3cc83bcbd8df9d12434e9488de76
dwkim <dwkim@uclick.co.kr>
2021-07-04 16:48:16 +0900

    11021 A+B - 7

---
+++ example/210704_for_07.cs.ex
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            StringBuilder result = new StringBuilder();
+
+            string[] input = new string[2];
+            for (int i=1; i<=T; i++) {
+                input = Console.ReadLine().Split(' ');
+                result.Append("Case #");
+                result.Append(i);
+                result.Append(": ");
+                result.Append(int.Parse(input[0]) + int.Parse(input[1]));
+                result.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
