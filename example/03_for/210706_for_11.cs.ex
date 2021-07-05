8de551140402ec12a15e175736977897d4bb0ff8
dwkim <dwkim@uclick.co.kr>
2021-07-05 20:37:23 +0900

    10871 X보다 작은 수

---
+++ example/03_for/210706_for_11.cs.ex
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = new string[2];
+            input = Console.ReadLine().Split(" ");
+
+            string[] array = new string[int.Parse(input[0])];
+            array = Console.ReadLine().Split(" ");
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i=0; i<array.Length; i++) {
+                if(int.Parse(array[i]) < int.Parse(input[1])) {
+                    result.Append(array[i] + " ");
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
