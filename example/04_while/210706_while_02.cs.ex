7750e7dc42546cc2ca2c03d16263713988919e0f
dwkim <dwkim@uclick.co.kr>
2021-07-05 20:47:51 +0900

    10951 A+B - 4

---
+++ example/04_while/210706_while_02.cs.ex
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+                string input = Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(input)) {
+                    break;
+                }
+                string[] array = input.Split();
+                Console.WriteLine(int.Parse(array[0]) + int.Parse(array[1]));
+            }
+        }
+    }
+}
