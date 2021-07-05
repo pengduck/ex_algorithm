debd08fc5d6f23b9e77c14e6e07d0ec12dbeccc8
dwkim <dwkim@uclick.co.kr>
2021-07-05 20:37:54 +0900

    10952 A+B - 5

---
+++ example/04_while/210706_while_01.cs.ex
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+                string[] input = Console.ReadLine().Split();
+                if(input[0] == "0" && input[1] == "0") {
+                    break;
+                }
+                Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
+            }
+        }
+    }
+}
