190c980b80e4f20c83321e1ab033bbd5d1936325
dwkim <dwkim@uclick.co.kr>
2021-07-06 19:58:31 +0900

    1110 더하기 사이클

---
+++ example/04_while/210706_while_03.cs.ex
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            int temp = i;
+            int count = 1;
+            while(true) {
+                i = ((i%10)*10) + (((i/10) + (i%10)) % 10);
+                if(temp == i) break;
+                else count++;
+            }
+            
+            Console.WriteLine(count);
+        }
+    }
+}
