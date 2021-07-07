a8ae28511cbc7d7f4a90f307055504bd743878b3
dwkim <dwkim@uclick.co.kr>
2021-07-07 16:04:47 +0900

    2562 최댓값

---
+++ example/05_array(1d)/210707_array_02.cs.ex
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[9];
+
+            int count = 0;
+            int max = 0;
+
+            for (int i=0; i<9; i++) {
+                array[i] = int.Parse(Console.ReadLine());
+                if(max < array[i]) {
+                    max = array[i];
+                    count = i+1;
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            result.Append(max + Environment.NewLine + count);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
