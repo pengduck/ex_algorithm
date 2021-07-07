c1da6e2844b1f6a08b4769bea88188cb74267149
dwkim <dwkim@uclick.co.kr>
2021-07-07 16:04:57 +0900

    2577 숫자의 개수

---
+++ example/05_array(1d)/210707_array_03.cs.ex
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[3];
+            
+            for(int i=0; i<3; i++) {
+                input[i] = int.Parse(Console.ReadLine());
+            }
+
+            string a = (input[0] * input[1] * input[2]).ToString();
+
+            StringBuilder result = new StringBuilder();
+            for(int j=0; j<10; j++) {
+                int count = 0;
+                for(int k=0; k<a.Length; k++) {
+                    if(int.Parse(a[k].ToString()) == j) count++;
+                }
+                result.Append(count + Environment.NewLine);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
