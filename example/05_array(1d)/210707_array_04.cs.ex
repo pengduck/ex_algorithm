40c2d525a3f8fcb845699605c7153fc79f62c47c
dwkim <dwkim@uclick.co.kr>
2021-07-07 18:13:07 +0900

    3052 나머지

---
+++ example/05_array(1d)/210707_array_04.cs.ex
@@ -0,0 +1,26 @@
+﻿using System;
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int inputCount = 10;
+            const int div = 42;
+            int reminder = 0;
+            bool[] check = new bool[div];
+            int count = 0;
+
+            for(int i=0; i<inputCount; i++) {
+                reminder = int.Parse(Console.ReadLine()) % div;
+                check[reminder] = true;
+            }
+            
+            foreach (var item in check) {
+                if(item == true) count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
