a2ef4e949daeb0e68d3567ff570d32fe386e9ae4
dwkim <dwkim@uclick.co.kr>
2021-07-08 21:02:53 +0900

    8958 OX퀴즈

---
+++ example/05_array(1d)/210708_array_06.cs.ex
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int point = 0;
+            int sum = 0;
+            for(int i=0; i<count; i++) {
+                string str = Console.ReadLine();
+                for(int j=0; j<str.Length; j++) {
+                    if(str[j] == 'O') {
+                        point++;
+                        sum += point;
+                    } else {
+                        point = 0;
+                    }
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+                point = 0;
+            }
+        }
+    }
+}
