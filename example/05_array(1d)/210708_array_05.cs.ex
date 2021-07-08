b6c77f98fb289357911fcb328a4e93f21d43c498
dwkim <dwkim@uclick.co.kr>
2021-07-08 20:20:39 +0900

    1546 평균

---
+++ example/05_array(1d)/210708_array_05.cs.ex
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int subj = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split();
+
+            int max = 0;
+
+            for(int i=0; i<subj; i++) {
+                if(max < int.Parse(input[i])) {
+                    max = int.Parse(input[i]);
+                }
+            }
+
+            float sum = 0;
+            for(int i=0; i<subj; i++) {
+                sum += float.Parse(input[i]) / max * 100;
+            }
+
+            Console.WriteLine(sum / subj);
+        }
+
+        
+    }
+}
