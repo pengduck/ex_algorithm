819422e62349fca4bfe72c7f57fbc39210fa1a20
dwkim <dwkim@uclick.co.kr>
2021-07-10 14:38:20 +0900

    1065 한수

---
+++ example/06_func/210710_func_03.cs.ex
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(HanSu(input));
+        }
+
+        static int HanSu(int num) {
+            int count = 0;
+            for(int i=1; i<=num; i++) {
+                if(i >= 100) {
+                    int hund = i / 100;
+                    int ten = (i / 10) % 10;
+                    int one = i % 10;
+
+                    if(hund-ten == ten-one) count++;
+                } else count++;
+            }
+
+            return count;
+        }
+    }
+}
