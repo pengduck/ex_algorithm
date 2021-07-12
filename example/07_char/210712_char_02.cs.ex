13229af706cc10b285e2b0cf2589c2c193f10ced
dwkim <dwkim@uclick.co.kr>
2021-07-12 21:03:11 +0900

    11720 숫자의 합

---
+++ example/07_char/210712_char_02.cs.ex
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            
+            string num = Console.ReadLine();
+            int result = 0;
+            for(int i=0; i<input; i++) {
+                result += num[i] - '0';
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
+
