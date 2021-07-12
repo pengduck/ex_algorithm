04e2086134fc9acb7850dbef4d98144163360b27
dwkim <dwkim@uclick.co.kr>
2021-07-12 21:03:51 +0900

    1157 단어 공부

---
+++ example/07_char/210712_char_05.cs.ex
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToUpper();
+            int[] count = new int[26];
+            int max = 0;
+            char mostUsed = 'A';
+
+            for(char c='A'; c<='Z'; c++) {
+                for (int i=0; i<input.Length; i++) {
+                    if(input[i] == c) count[c-65]++;
+                }
+                if(count[c-65] > max) {
+                    max = count[c-65];
+                    mostUsed = c;
+                };
+            }
+
+            bool condition1 = false;
+            bool condition2 = false;
+            for(int i=0; i<26; i++) {
+                if(condition1 == true && count[i] == max) {
+                    condition2 = true;
+                } else if(count[i] == max) {
+                    condition1 = true;
+                }
+            }
+
+            if(condition2) {
+                Console.WriteLine("?");
+            } else {
+                Console.WriteLine(mostUsed.ToString());
+            }
+
+        }
+    }
+}
