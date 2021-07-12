bb019683b22ac97170ff5490b760914378d819a5
dwkim <dwkim@uclick.co.kr>
2021-07-12 21:03:39 +0900

    2675 문자열 반복

---
+++ example/07_char/210712_char_04.cs.ex
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<input; i++) {
+                string[] str = Console.ReadLine().Split();
+                foreach(var j in str[1]) {
+                    for(int k=0; k<int.Parse(str[0]); k++) {
+                        sb.Append(j.ToString());
+                    }
+                }
+                sb.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
