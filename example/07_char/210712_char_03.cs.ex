7982b1b3830ce262334b4b1537038bcc72611760
dwkim <dwkim@uclick.co.kr>
2021-07-12 21:03:26 +0900

    10809 알파벳 찾기

---
+++ example/07_char/210712_char_03.cs.ex
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string word = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            var count = 0;
+            for(char i='a'; i<='z'; i++) {
+                for(int j=0; j<word.Length; j++) {
+                    if(word[j] == i) {
+                        sb.Append(count.ToString() + " ");
+                        break;
+                    } else if(j == word.Length - 1) {
+                        sb.Append((-1).ToString() + " ");
+                    }
+                    count++;
+                }
+                count = 0;
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
