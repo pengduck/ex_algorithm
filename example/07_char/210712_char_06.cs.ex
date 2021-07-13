1fbf1dd077ba18dc2504adb820ccf99984428245
dwkim <dwkim@uclick.co.kr>
2021-07-13 19:35:02 +0900

    1152 단어의 개수

---
+++ example/07_char/210712_char_06.cs.ex
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Trim().Split(" ");
+
+            if(string.IsNullOrEmpty(input[0])) Console.WriteLine(0); 
+            else Console.WriteLine(input.Length);
+        }
+    }
+}
