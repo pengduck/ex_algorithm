5878c93ff6819da1080b859a5c1fdc28ee397873
dwkim <dwkim@uclick.co.kr>
2021-07-13 20:14:28 +0900

    2908 상수

---
+++ example/07_char/210713_char_07.cs.ex
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] compare = new int[2];
+
+            for (int j=0; j<2; j++) {
+                for (int i=input[j].Length-1; i>=0; i--) {
+                    compare[j] += (input[j][i] - '0') * (int) Math.Pow(10, i);
+                }
+            }
+
+            if(compare[0] > compare[1]) Console.WriteLine(compare[0]);
+            else Console.WriteLine(compare[1]);
+
+        }
+    }
+}
