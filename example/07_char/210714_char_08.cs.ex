9f18ef9a33ce56fd1f439a463178c456765d0a21
dwkim <dwkim@uclick.co.kr>
2021-07-14 20:45:17 +0900

    5622 다이얼

---
+++ example/07_char/210714_char_08.cs.ex
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToUpper();
+
+            int result = 0;
+            foreach(var c in input) {
+                result += seconds(c);
+            }
+            Console.WriteLine(result);
+        }
+
+        static int seconds(char c) {
+            int s = 2;
+            if(c >= 'A') s++;
+            if(c >= 'D') s++;
+            if(c >= 'G') s++;
+            if(c >= 'J') s++;
+            if(c >= 'M') s++;
+            if(c >= 'P') s++;
+            if(c >= 'T') s++;
+            if(c >= 'W') s++;
+            if(c > 'Z') s++;
+
+            return s;
+        }
+    }
+}
