9f746a376dd2d48ab942f4ed851ef796e5bde6ba
dwkim <dwkim@uclick.co.kr>
2021-07-16 20:35:45 +0900

    1712 손익분기점

---
+++ example/08_math_1/210716_math_01.cs.ex
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int fixedCost = int.Parse(input[0]);
+            int variable = int.Parse(input[1]);
+            int price = int.Parse(input[2]);
+
+            int sales = 1;
+            if(variable >= price) {
+                sales = -1;
+            } else {
+                sales = fixedCost / (price - variable);
+                sales++;
+            }
+            Console.WriteLine(sales);
+        }
+    }
+}
