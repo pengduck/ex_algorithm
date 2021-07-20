d7fb502fd9537d24987b4596675d7f87dd604acb
dwkim <dwkim@uclick.co.kr>
2021-07-20 20:12:09 +0900

    1193 분수찾기

---
+++ example/08_math_1/210717_math_02.cs.ex
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int count = 1;
+            int num = 1;
+            while (input > num) {
+                num += 6 * count;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
