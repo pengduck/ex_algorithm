5139d7de6523088d1547c8201ee7993f7d9cb611
dwkim <dwkim@uclick.co.kr>
2021-07-20 21:34:32 +0900

    2869 달팽이는 올라가고 싶다

---
+++ example/08_math_1/210720_math_04.cs.ex
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int up = int.Parse(input[0]);
+            int down = int.Parse(input[1]);
+            int height = int.Parse(input[2]);
+
+            int count = (height - up) / (up - down) + ((height-up) % (up - down) == 0 ? 1 : 2);
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
