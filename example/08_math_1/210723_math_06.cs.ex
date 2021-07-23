76a49524fa6bd856ff7197023076707b3eeb4828
dwkim <dwkim@uclick.co.kr>
2021-07-23 20:14:29 +0900

    2775 부녀회장이 될테야

---
+++ example/08_math_1/210723_math_06.cs.ex
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            int k = 0;
+            StringBuilder sb = new StringBuilder();
+            int prev = 1;
+            for(int a=1; a<=t; a++) {
+                k = int.Parse(Console.ReadLine());
+                int[] resident = new int[int.Parse(Console.ReadLine())];
+                for(int i=0; i<resident.Length; i++) {
+                    resident[i] = i+1;
+                }
+
+                for(int b=1; b<=k; b++) {
+                    for(int c=1; c<resident.Length; c++) {
+                        resident[c] = resident[c] + prev;
+                        prev = resident[c];
+                    }
+                    prev = 1;
+                }
+                sb.Append(resident[resident.Length-1] + Environment.NewLine);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
