dec2c26ee30a80fecd6f75d7a4ed033b41a6a41b
dwkim <dwkim@uclick.co.kr>
2021-07-23 21:18:45 +0900

    2839 설탕 배달

---
+++ example/08_math_1/210723_math_07.cs.ex
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(count(n));
+        }
+
+        static int count(int n) {
+            if(n < 3) return -1;
+            int shareFive = n / 5;
+
+            while (shareFive >=0) {
+                int left = n - (shareFive * 5);
+                if(left % 3 == 0) {
+                    return shareFive + (left / 3);
+                }
+                shareFive--;
+            }
+
+            return -1;
+        }
+    }
+}
