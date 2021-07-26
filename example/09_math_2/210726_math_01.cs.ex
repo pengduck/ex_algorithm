5617754a0664a7fdb13923dec049e14b68a6826a
dwkim <dwkim@uclick.co.kr>
2021-07-26 19:39:29 +0900

    1978 소수 찾기

---
+++ example/09_math_2/210726_math_01.cs.ex
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string[] numList = new string[input];
+            numList = Console.ReadLine().Split();
+
+            int count = 0;
+            
+            for(int i=0; i<input; i++) {
+                int num = int.Parse(numList[i]);
+                int div = 0;
+                for(int j=1; j<=num; j++) {
+                    if(num % j == 0) div++;
+                }
+                if(div == 2) count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
