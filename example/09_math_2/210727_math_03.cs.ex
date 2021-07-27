10436ad7dd327fefbfa0a1b4b1b591d28894a486
dwkim <dwkim@uclick.co.kr>
2021-07-27 20:45:54 +0900

    11653 소인수분해

---
+++ example/09_math_2/210727_math_03.cs.ex
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int endNum = (int) Math.Sqrt(num);
+            for (int i = 2; i <= endNum; i++) {
+                while(num % i == 0) {
+                    Console.WriteLine(i);
+                    num /= i;
+                }
+            }
+
+            if(num != 1) {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
