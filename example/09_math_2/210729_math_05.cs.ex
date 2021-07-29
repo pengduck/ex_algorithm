e3d2114b8814ce35982303c7af912baa311e77b5
dwkim <dwkim@uclick.co.kr>
2021-07-29 20:51:38 +0900

    4948 베르트랑 공준

---
+++ example/09_math_2/210729_math_05.cs.ex
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static int max = 123456;
+        static void Main(string[] args)
+        {
+            bool[] notPrimes = new bool[max * 2 + 1];
+
+            notPrimes[1] = true;
+            int end = (int) Math.Sqrt(max * 2);
+            for(int i=2; i<=end; i++) {
+                if(notPrimes[i]) continue;
+                for(int j=2; j*i<=max*2; j++) {
+                    notPrimes[i * j] = true;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int input = int.Parse(Console.ReadLine());
+            int count = 0;
+            while(input != 0) {
+                for(int i=input+1; i<=input*2; i++) {
+                    if(!notPrimes[i]) count++;
+                }
+                sb.AppendLine(count.ToString());
+                count = 0;
+                input = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+    }
+}
