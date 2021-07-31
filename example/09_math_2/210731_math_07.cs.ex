8fa4f3df8c0c05f37f46d6b3f6b256feec9e659f
dwkim <dwkim@uclick.co.kr>
2021-07-31 12:51:21 +0900

    1085 직사각형에서 탈출

---
+++ example/09_math_2/210731_math_07.cs.ex
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] square = Console.ReadLine().Split(" ");
+            int x = int.Parse(square[0]);
+            int y = int.Parse(square[1]);
+            int distanceX = int.Parse(square[2]) - x;
+            int distanceY = int.Parse(square[3]) - y;
+            
+            int[] distances = {x, y, distanceX, distanceY};
+
+            Console.WriteLine(distances.Min());
+        }
+    }
+}
