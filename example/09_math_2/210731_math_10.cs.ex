a1f07a8e368580a42394304318e967d4a845eee5
dwkim <dwkim@uclick.co.kr>
2021-07-31 16:07:36 +0900

    3053 택시 기하학

---
+++ example/09_math_2/210731_math_10.cs.ex
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine((r * r * Math.PI).ToString());
+            sb.AppendLine((r * r * 2).ToString());
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
