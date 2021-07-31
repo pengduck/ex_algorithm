36391516dc968a5256bacba8ace4e674b0b969e3
dwkim <dwkim@uclick.co.kr>
2021-07-31 17:01:34 +0900

    10870 피보나치 수 5

---
+++ example/10_recursion/210731_recursion_02.cs.ex
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(fibonacci(input));
+        }
+
+        static int fibonacci(int i) {
+            if(i == 0) return 0;
+            if(i == 1) return 1;
+            return fibonacci(i-1) + fibonacci(i-2);
+        }
+    }
+}
