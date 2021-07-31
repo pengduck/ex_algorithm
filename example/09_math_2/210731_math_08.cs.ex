c46f44287eef624a0d7f3de4052611609ae12996
dwkim <dwkim@uclick.co.kr>
2021-07-31 15:30:47 +0900

    3009 네 번째 점

---
+++ example/09_math_2/210731_math_08.cs.ex
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(" ");
+            string[] input2 = Console.ReadLine().Split(" ");
+            string[] input3 = Console.ReadLine().Split(" ");
+
+            int x = onlyOnce(int.Parse(input1[0]), int.Parse(input2[0]), int.Parse(input3[0]));
+            int y = onlyOnce(int.Parse(input1[1]), int.Parse(input2[1]), int.Parse(input3[1]));
+
+            Console.WriteLine(x + " " + y);
+        }
+
+        static int onlyOnce(int i, int j, int k) {
+            if(i == j) {
+                return k;
+            } else if (i == k) {
+                return j;
+            } else {
+                return i;
+            }
+        }
+    }
+}
