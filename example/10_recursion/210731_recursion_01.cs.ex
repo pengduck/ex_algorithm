3968fe0ab29bf2544e2c9204fd4846afdea4fd9a
dwkim <dwkim@uclick.co.kr>
2021-07-31 16:55:01 +0900

    10872 팩토리얼

---
+++ example/10_recursion/210731_recursion_01.cs.ex
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(factorial(input));
+        }
+
+        static int factorial(int i) {
+            if(i < 2) return 1;
+            return i * factorial(i-1);
+        }
+    }
+}
