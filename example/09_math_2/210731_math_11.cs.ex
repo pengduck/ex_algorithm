813b2769afe7c1a2ec162e531fdd4303b2d15ecb
dwkim <dwkim@uclick.co.kr>
2021-07-31 16:38:33 +0900

    1002 터렛

---
+++ example/09_math_2/210731_math_11.cs.ex
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<input; i++) {
+                string[] turrets = Console.ReadLine().Split();
+
+                int x1 = int.Parse(turrets[0]);
+                int y1 = int.Parse(turrets[1]);
+                int r1 = int.Parse(turrets[2]);
+                int x2 = int.Parse(turrets[3]);
+                int y2 = int.Parse(turrets[4]);
+                int r2 = int.Parse(turrets[5]);
+
+                double distance = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+                if(distance > r1 + r2 || distance < Math.Abs(r1 - r2)) sb.AppendLine(0.ToString());
+                else if(distance < r1 + r2 && distance > Math.Abs(r1 - r2)) sb.AppendLine(2.ToString());
+                else if(distance == 0 && r1 == r2) sb.AppendLine((-1).ToString());
+                else sb.AppendLine(1.ToString());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
