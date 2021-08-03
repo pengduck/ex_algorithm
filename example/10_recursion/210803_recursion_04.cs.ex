f893446d8acc8fbc9d8ce9045124fd8436d3ef33
dwkim <dwkim@uclick.co.kr>
2021-08-03 21:05:40 +0900

    11729 하노이 탑 이동 순서

---
+++ example/10_recursion/210803_recursion_04.cs.ex
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static StringBuilder sb = new StringBuilder();
+        static int cnt = 0;
+
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            hanoi(input, 1, 2, 3);
+            sb.Insert(0, cnt.ToString() + Environment.NewLine);
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void hanoi(int i, int from, int via, int to) {
+            if(i == 1) {
+                sb.AppendLine(from + " " + to);
+                cnt++;
+            } else {
+                hanoi(i-1, from, to, via);
+                sb.AppendLine(from + " " + to);
+                cnt++;
+                hanoi(i-1, via, from, to);
+            }
+        }
+    }
+}
