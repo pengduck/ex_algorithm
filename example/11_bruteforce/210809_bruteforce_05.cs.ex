6e5e9506d8404880a785c8b01aae4d6d668d3df3
dwkim <dwkim@uclick.co.kr>
2021-08-11 20:14:11 +0900

    1436 영화감독 숌

---
+++ example/11_bruteforce/210809_bruteforce_05.cs.ex
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string num = "666";
+            int end = 665;
+            int cnt = 0;
+
+            while(true) {
+                end++;
+
+                if(end.ToString().Contains(num)) {
+                    cnt++;
+                };
+
+                if(cnt == input) break;
+            }
+
+            Console.WriteLine(end);
+        }
+
+        // static bool chk() {
+        //     return false;
+        // }
+    }
+}
