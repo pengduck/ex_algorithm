6bd51f0564ad1d67c16adf71cc7ecd5ca1ba00c6
dwkim <dwkim@uclick.co.kr>
2021-08-13 20:49:52 +0900

    2751 수 정렬하기 2

---
+++ example/12_sort/210811_sort_02.cs.ex
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] numbers = new int[input];
+            for(int i=0; i<input; i++) {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0; i<input; i++) {
+                sb.AppendLine(numbers[i].ToString());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+    }
+}
