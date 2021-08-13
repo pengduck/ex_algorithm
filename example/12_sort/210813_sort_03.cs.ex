9b4249e4548e6c966a9bd96e7573e24c2a648f9a
dwkim <dwkim@uclick.co.kr>
2021-08-13 20:50:03 +0900

    10989 수 정렬하기 3

---
+++ example/12_sort/210813_sort_03.cs.ex
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] numbers = new int[10001];
+            Array.Clear(numbers, 0, numbers.Length);
+            for(int i=0; i<input; i++) {
+                numbers[int.Parse(Console.ReadLine())]++;
+            }
+
+            var output = new System.IO.StreamWriter(Console.OpenStandardOutput());
+            for(int i=0; i<numbers.Length; i++) {
+                for(int j=0; j<numbers[i]; j++) {
+                    output.WriteLine(i);
+                }
+            }
+
+            output.Close();
+        }
+
+    }
+}
