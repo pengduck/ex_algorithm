f035ec2649c83e0a8f9ea94f10938c3dd89a7c99
dwkim <dwkim@uclick.co.kr>
2021-08-16 16:56:56 +0900

    11651 좌표 정렬하기 2

---
+++ example/12_sort/210816_sort_07.cs.ex
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        public class Coord : IComparable
+        {
+            public int x;
+            public int y;
+
+            public Coord(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public int CompareTo(Object obj) {
+                Coord c = (Coord)obj;
+                if(this.y == c.y) return this.x.CompareTo(c.x);
+                else return this.y.CompareTo(c.y);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int cnt = int.Parse(Console.ReadLine());
+            List<Coord> coordList = new List<Coord>();
+            for(int i=0; i<cnt; i++) {
+                string[] input = Console.ReadLine().Split();
+                coordList.Add(new Coord(int.Parse(input[0]), int.Parse(input[1])));
+            }
+
+            coordList.Sort();
+
+            var output = new System.IO.StreamWriter(Console.OpenStandardOutput());
+            for(int i=0; i<cnt; i++) {
+                output.WriteLine(coordList[i].x + " " + coordList[i].y);
+            }
+
+            output.Close();
+        }
+    }
+}
