495daaa7a55ec8cc46f24ee2f8658fe8a0b02243
dwkim <dwkim@uclick.co.kr>
2021-08-16 16:57:13 +0900

    1181 단어 정렬

---
+++ example/12_sort/210816_sort_08.cs.ex
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = int.Parse(Console.ReadLine());
+            List<string> wordList = new List<string>();
+            for(int i=0; i<cnt; i++) {
+                wordList.Add(Console.ReadLine());
+            }
+
+            var sortedList = wordList.Distinct().OrderBy(x => x.Length).ThenBy(x => x);
+
+            var output = new System.IO.StreamWriter(Console.OpenStandardOutput());
+            foreach(string word in sortedList) {
+                output.WriteLine(word);
+            }
+
+            output.Close();
+        }
+    }
+}
