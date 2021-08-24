2c45c6c3cba7a513c93f07da361aee9f7b16b6bf
dwkim <dwkim@uclick.co.kr>
2021-08-24 20:54:31 +0900

    18870 좌표 압축

---
+++ example/12_sort/210823_sort_10.cs.ex
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int cnt = int.Parse(Console.ReadLine());
+
+            List<int> origin = Console.ReadLine().Split().Select(x => Int32.Parse(x)).ToList();
+            List<int> sorting = new List<int>(origin).Distinct().ToList();
+            sorting.Sort();
+
+            var output = new System.IO.StreamWriter(Console.OpenStandardOutput());
+
+            for(int i=0; i<cnt; i++) {
+                output.Write(sorting.BinarySearch(origin[i]) + " ");
+            }
+
+            output.Close();
+        }
+    }
+}
