4ed94e14b9951709d3b21e6252abfed129ade89c
dwkim <dwkim@uclick.co.kr>
2021-09-06 19:57:19 +0900

    15649 N과 M (1)

---
+++ example/14_backtracking/210830_N과M_1.cs.ex
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace exAlgorithm
+{
+    class Program
+    {
+        static bool[] numbers;
+        static int[] chosen;
+        static int count;
+        static int toChoose;
+
+        static StringBuilder sb = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            count = input[0];
+            numbers = new bool[count];
+            toChoose = input[1];
+            chosen = new int[toChoose];
+
+            choose(0);
+        }
+
+        static void choose(int num) {
+            if(num == toChoose) {
+                for(int i=0; i<toChoose; i++) {
+                    sb.Append(chosen[i] + " ");
+                }
+                Console.WriteLine(sb.ToString());
+                sb.Clear();
+                return;
+            }
+
+            for(int i=0; i<count; i++) {
+                if(!numbers[i]) {
+                    numbers[i] = true;
+                    chosen[num] = i+1;
+                    choose(num+1);
+                    numbers[i] = false;
+                }
+            }
+        }
+    }
+}
